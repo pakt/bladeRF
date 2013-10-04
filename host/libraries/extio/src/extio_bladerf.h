91ea10f2582ff5ab2f38deef7cc665f463e296e7
Jon Szymaniak <jon.szymaniak@gmail.com>
2013-10-04 19:57:41 -0400

    extio: Updated library name, removed extra 'extern "C"' items

--- host/libraries/extio/src/extio_bladerf.h
+++ host/libraries/extio/src/extio_bladerf.h
@@ -1,3 +1,11 @@
+/* 
+ * bladeRF ExtIO interface
+ *
+ * See the following document for a specification of the
+ * the ExtIO interface:
+ *      http://www.winrad.org/bin/Winrad_Extio.pdf
+ *
+ */
 #ifndef EXTIO_BLADERF_H
 #define EXTIO_BLADERF_H
 
@@ -8,51 +16,45 @@ extern "C" {
 #define EXTIO_API __declspec(dllexport) __stdcall
 #include <stdint.h>
 
-/* Function prototypes taken from:
- *
- *  http://www.winrad.org/bin/Winrad_Extio.pdf
- *
- */
-
 /* Required functions */
-extern "C" bool     EXTIO_API InitHW(char *name, char *model, int& type);
-extern "C" bool     EXTIO_API OpenHW(void);
-extern "C" int      EXTIO_API StartHW(long freq);
-extern "C" int64_t  EXTIO_API StartHW64(int64_t freq);
-extern "C" void     EXTIO_API StopHW(void);
-extern "C" void     EXTIO_API CloseHW(void);
-extern "C" int      EXTIO_API SetHWLO(long LOfreq);
-extern "C" int64_t  EXTIO_API SetHWLO64(int64_t LOfreq);
-extern "C" int      EXTIO_API GetStatus(void);
-extern "C" void     EXTIO_API SetCallback(void (*callback)(int, int, float, short *));
+bool     EXTIO_API InitHW(char *name, char *model, int& type);
+bool     EXTIO_API OpenHW(void);
+int      EXTIO_API StartHW(long freq);
+int64_t  EXTIO_API StartHW64(int64_t freq);
+void     EXTIO_API StopHW(void);
+void     EXTIO_API CloseHW(void);
+int      EXTIO_API SetHWLO(long LOfreq);
+int64_t  EXTIO_API SetHWLO64(int64_t LOfreq);
+int      EXTIO_API GetStatus(void);
+void     EXTIO_API SetCallback(void (*callback)(int, int, float, short *));
 
 /* Optional Functions */
-extern "C" long     EXTIO_API GetHWLO(void);
-extern "C" int64_t  EXTIO_API GetHWLO64(void);
-extern "C" long     EXTIO_API GetHWSR(void);
-extern "C" long     EXTIO_API GetTune(void);
-extern "C" void     EXTIO_API GetFilters(int& loCut, int* hiCut, int& pitch);
-extern "C" char     EXTIO_API GetMode(void);
-extern "C" void     EXTIO_API ModeChanged(char mode);
-extern "C" void     EXTIO_API ShowGUI(void);
-extern "C" void     EXTIO_API HideGUI(void);
-extern "C" void     EXTIO_API IFLimitsChanged(long low, long high);
-extern "C" void     EXTIO_API TuneChanged(long freq);
-extern "C" void     EXTIO_API RawDataReady(long samprate, int *Ldata, int *Rdata, int numsamples);
+long     EXTIO_API GetHWLO(void);
+int64_t  EXTIO_API GetHWLO64(void);
+long     EXTIO_API GetHWSR(void);
+long     EXTIO_API GetTune(void);
+void     EXTIO_API GetFilters(int& loCut, int* hiCut, int& pitch);
+char     EXTIO_API GetMode(void);
+void     EXTIO_API ModeChanged(char mode);
+void     EXTIO_API ShowGUI(void);
+void     EXTIO_API HideGUI(void);
+void     EXTIO_API IFLimitsChanged(long low, long high);
+void     EXTIO_API TuneChanged(long freq);
+void     EXTIO_API RawDataReady(long samprate, int *Ldata, int *Rdata, int numsamples);
 
-extern "C" void     EXTIO_API VersionInfo(const char * progname, int ver_major, int ver_minor);
+void     EXTIO_API VersionInfo(const char * progname, int ver_major, int ver_minor);
 
-extern "C" int      EXTIO_API GetAttenuators(int idx, float * attenuation);
-extern "C" int      EXTIO_API GetActualAttIdx(void);
-extern "C" int      EXTIO_API SetAttenuator(int idx);
+int      EXTIO_API GetAttenuators(int idx, float * attenuation);
+int      EXTIO_API GetActualAttIdx(void);
+int      EXTIO_API SetAttenuator(int idx);
 
-extern "C" int      EXTIO_API ExtIoGetSrates(int idx, double * samplerate);
-extern "C" int      EXTIO_API ExtIoGetActualSrateIdx(void);
-extern "C" int      EXTIO_API ExtIoSetSrate(int idx);
-extern "C" long     EXTIO_API ExtIoGetBandwidth(int srate_idx);
+int      EXTIO_API ExtIoGetSrates(int idx, double * samplerate);
+int      EXTIO_API ExtIoGetActualSrateIdx(void);
+int      EXTIO_API ExtIoSetSrate(int idx);
+long     EXTIO_API ExtIoGetBandwidth(int srate_idx);
 
-extern "C" int      EXTIO_API ExtIoGetSetting( int idx, char * description, char * value );
-extern "C" void     EXTIO_API ExtIoSetSetting( int idx, const char * value );
+int      EXTIO_API ExtIoGetSetting( int idx, char * description, char * value );
+void     EXTIO_API ExtIoSetSetting( int idx, const char * value );
 
 #ifdef __cplusplus
 }
